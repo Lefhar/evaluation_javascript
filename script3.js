4d89b0d7a5ccda3bee2a0e7aa9eda72f1657cf64
harold <s.lefebvre907@laposte.net>
2020-11-14 19:26:41 +0100

    evaluation javascript

--- script3.js
+++ script3.js
@@ -6,10 +6,13 @@ var saisi = window.prompt("voici la liste des prénoms "+tab+" entrer le prénom
 var suprenommin = String(saisi).charAt(0).toUpperCase() + String(saisi).slice(1).toLowerCase();// on met la premiére lettre en majuscule et le reste du mot en minuscule
 
 //maintenant on lance la recherche l'effacement et le retour du tableau
-if(tab.includes(suprenommin)){// on lance la recherche du mot saisi par suprenommin
+if(tab.includes(suprenommin))// on lance la recherche du mot saisi par suprenommin
+{
+
 	tab.splice(tab.indexOf(suprenommin),1);// on supprime par rapport l'index
 	tab.push(" ");//on le remplace par un espace vide
-alert("Voici la nouvelle liste "+tab)//on affiche le resultat
+
+	alert("Voici la nouvelle liste "+tab)//on affiche le resultat
 	console.log(tab)//on affiche dans la console le resultat
 }
 else
